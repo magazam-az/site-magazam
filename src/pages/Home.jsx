ce095132aae5715d2e9e4437dff47604cd9487e7
Aga-42i <rahim.bahsiyev@gmail.com>
2025-11-09 21:40:55 +0400

    Ipad section skelet +

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,15 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HomePage from '../components/Hero'
 import FAQPage from '../components/FAQ'
+import IpadSection from '../components/IpadSection'
 
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
-        {/* <HomePage/> */}
+        <HomePage/>
+        <IpadSection/>
         <FAQPage/>
         <Footer/>
     </div>
