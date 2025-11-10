188fcbe56f9402631ce4fceaa7ea51562937d964
Aga-42i <rahim.bahsiyev@gmail.com>
2025-11-10 22:17:26 +0400

    example change (Not Informative)

---
+++ src/ui/Container.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Kiçik utility funksiyası: class-ları birləşdirmək üçün
+function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Container = ({ children, className }) => {
+  return (
+    <div className={cn("max-w-screen-xl mx-auto px-4", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
